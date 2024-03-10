4a4f25f4f7bc607139c85c1b04ed5ba2a2db1b4f
Artur Tonyan <atonyan172@gmail.com>
2024-03-10 13:58:20 +0300

    project init

---
+++ Python_bot/db.py
@@ -0,0 +1,10 @@
+import psycopg2
+import config
+
+try:
+    # пытаемся подключиться к базе данных
+    conn = psycopg2.connect(config.DB_URL)
+    print('connection is ok')
+except:
+    # в случае сбоя подключения будет выведено сообщение в STDOUT
+    print('Can`t establish connection to database')
