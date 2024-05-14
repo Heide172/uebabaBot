5d869178115152630ba7042ffa920652d67bd09e
Arthur Tonyan <atonyan172@gmail.com>
2024-05-14 13:26:14 +0300

    uvu fix

--- python_bot/bot.py
+++ python_bot/bot.py
@@ -107,7 +107,7 @@ async def yvy_command(message: types.Message):
             return
         else:
             db.update_user_uvu_count(count + 1, user.id)
-            if (random.randrange(0,100) == 0):
+            if (random.randrange(0,50) == 0):
                 await message.answer_sticker(r'CAACAgIAAxkBAAEFQMRmOjyP3TzJgZwh9fHNMm8gGqaFtwACjS0AAiq1EUjDQfFev1DanzUE') 
             else: 
                 await message.answer(text='увы')
